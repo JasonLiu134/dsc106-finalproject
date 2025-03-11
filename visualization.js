84b41d07993cacac1dfab8cfaa027e872aa543bb
Ishaan Kor <ishaankor@gmail.com>
2025-03-11 06:02:54 -0700

    Added the ML model to the final visualization.

--- visualization.js
+++ visualization.js
@@ -75,33 +75,40 @@ function createFinalVis() {
       .attr("x", 50)
       .attr("y", 30)
       .attr("height", 40)
-      .attr("width", widthScale(3))
+      .attr("width", widthScale(14))
       .attr("fill", "black")
       .attr("class", "fillbox");
   
     // Create text
     const label = svg.append("text")
-      .attr("x", 50 + widthScale(3) + 10)
+      .attr("x", 50 + widthScale(14) + 10)
       .attr("y", 80)
       .attr("dy", ".35em")
-      .text("3 days");
+      .text("14 days");
   
-    document.getElementById("glucose").addEventListener("input", updateDisplay);
-    document.getElementById("albumin").addEventListener("input", updateDisplay);
-    document.getElementById("alt").addEventListener("input", updateDisplay);
-  
+    document.getElementById("sodium").addEventListener("input", updateDisplay);
+    document.getElementById("hemoglobin").addEventListener("input", updateDisplay);
+    document.getElementById("platelets").addEventListener("input", updateDisplay);
+    document.getElementById("aptt").addEventListener("input", updateDisplay);
+    document.getElementById("ast").addEventListener("input", updateDisplay);
+
     function updateDisplay() {
-      let glucose = parseFloat(document.getElementById("glucose").value);
-      let alb = parseFloat(document.getElementById("albumin").value);
-      let alt = parseFloat(document.getElementById("alt").value);
+      let sodium = parseFloat(document.getElementById("sodium").value);
+      let hemoglobin = parseFloat(document.getElementById("hemoglobin").value);
+      let platelets = parseFloat(document.getElementById("platelets").value);
+      let aptt = parseFloat(document.getElementById("aptt").value);
+      let ast = parseFloat(document.getElementById("ast").value);
     
       // Example regression line equation (will replace with actual model coefficients)
-      let icu_days = 0.02 * glucose - 1.5 * alb + 0.03 * alt + 5;
+      let icu_days = (0.029775145236927203 * aptt) + (-0.13463185618371287 * sodium) + (-0.20034793436033685 * hemoglobin) + (-0.004446836926403632 * platelets) + (0.003035309694611115 * ast) + 21.920014027366662;
+      // console.log(icu_days);
       icu_days = Math.max(0, Math.min(14, icu_days.toFixed(1)));
     
-      document.getElementById("glucoseVal").innerText = glucose;
-      document.getElementById("albuminVal").innerText = alb;
-      document.getElementById("altVal").innerText = alt;
+      document.getElementById("sodiumVal").innerText = sodium;
+      document.getElementById("hbVal").innerText = hemoglobin;
+      document.getElementById("pltVal").innerText = platelets;
+      document.getElementById("apttVal").innerText = aptt;
+      document.getElementById("astVal").innerText = ast;
       document.getElementById("icuPred").innerText = icu_days;
     
       // Update bar width
